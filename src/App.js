f241bce2d4506a80c5a1ef610197e672f12b70b0
GuillaumeCARRA <carraguillaume@gmail.com>
2022-02-10 12:17:06 +0100

    component initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import SearchBar from '../src/components/SearchBar';
+import NumberRepos from '../src/components/NumberRepos';
+import ShowRepos from '../src/components/ShowRepos'; 
+
+import repos from '../src/data/repos'; 
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      <NumberRepos />
+      <ShowRepos />
     </div>
   );
 }
---
+++ src/components/ShowRepos/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShowRepos() {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default ShowRepos;
